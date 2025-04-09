a5e62de12cf2734405590980ad0d4ca220f06bad
Björn Stålfors <bjorn.stalfors@im.se>
2025-04-09 14:07:33 +0200

    wip

--- UtbildningTDD.Tests/GameTests.cs
+++ UtbildningTDD.Tests/GameTests.cs
@@ -31,9 +31,35 @@ public class GameTests
         
         sut.Roll(1);
         sut.Roll(1);
+        
         var result = sut.Score();
         
         Assert.Equal(2, result);
+    }
+    
+    [Fact]
+    public void ThreeSingleRollsIsThree()
+    {
+        var sut = new Game();
+        
+        sut.Roll(1);
+        sut.Roll(1);
+        sut.Roll(1);
+        var result = sut.Score();
+        
+        Assert.Equal(3, result);
+    }
+
+    [Fact]
+    public void ThreeSingleRollsAdvancesFrame()
+    {
+        var sut = new Game();
+        
+        sut.Roll(1);
+        sut.Roll(1);
+        sut.Roll(1);
+        
+        Assert.Equal(1, sut.CurrentFrame);
     }
     
     [Fact]
@@ -48,5 +74,39 @@ public class GameTests
         Assert.Equal(0, result);
     }
 
+    [Fact]
+    public void SparePlusNextRollReturnsScore()
+    {
+        var sut = new Game();
+        
+        sut.Roll(1);
+        sut.Roll(9);
+        sut.Roll(1);
+        var result = sut.Score();
+        
+        Assert.Equal(12, result);
+    }
+
+    [Fact]
+    public void OneStrikePlusTwoOnesIs14()
+    {
+        var sut = new Game();
+        sut.Roll(10);
+        sut.Roll(1);
+        sut.Roll(1);
+        var result = sut.Score();
+        
+        Assert.Equal(14, result);
+    }
     
+    [Fact]
+    public void OneStrikePlusOnesIs0()
+    {
+        var sut = new Game();
+        sut.Roll(10);
+        sut.Roll(1);
+        var result = sut.Score();
+        
+        Assert.Equal(0, result);
+    }    
 }
--- UtbildningTDD/Game.cs
+++ UtbildningTDD/Game.cs
@@ -1,16 +1,72 @@
 namespace UtbildningTDD;
 
-public class 
+public class Game
 {
-    private int rolls;
+    private readonly Frame[] frames = new Frame[10];
+    private int currentFrame;
+    private bool frameComplete;
+
+    public Game()
+    {
+        currentFrame = 0;
+        for (var i = 0; i < 10; i++)
+        {
+            frames[i] = new Frame(i);
+        }
+        
+        for (var i = 0; i < 9; i++)
+        {
+            frames[i].NextFrame = frames[i + 1];
+        }
+    }
+    
+    public int CurrentFrame => currentFrame;
     
     public int Score()
     {
-        return rolls;
+        return frames.Sum(x => x.Score());
     }
 
     public void Roll(int roll)
     {
-        rolls += roll;
+        frameComplete = frames[currentFrame].Roll(roll);
+        if (frameComplete)
+        {
+            currentFrame++;
+        }
+    }
+}
+
+internal class Frame(int index)
+{
+    private readonly List<int> rolls = [];
+    
+    public Frame? NextFrame { get; set; }
+
+    public bool Roll(int roll)
+    {
+        rolls.Add(roll);
+        return IsStrike || rolls.Count == 2;
+    }
+    
+    private bool IsSpare => rolls.Count == 2 && rolls.Sum() == 10;
+    
+    private bool IsStrike => rolls.Count == 1 && rolls.Sum() == 10;
+    
+    private bool HasRolls => rolls.Count > 0;
+
+    public int Score()
+    {
+        if (IsSpare && NextFrame is not null && NextFrame.HasRolls)
+        {
+            return 10 + NextFrame.rolls[0];
+        }
+        
+        if (IsStrike && NextFrame is not null && NextFrame.HasRolls)
+        {
+            return 10 + NextFrame.rolls[0] + NextFrame.rolls[1];
+        }        
+
+        return IsSpare ? 0 : rolls.Sum();
     }
 }
